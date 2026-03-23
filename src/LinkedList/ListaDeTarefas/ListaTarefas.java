2e7fc513c8a0f542ad28acd8371643f8027df3ef
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-03-23 07:48:11 -0300

    Estudos Linked List

--- src/LinkedList/ListaDeTarefas/ListaTarefas.java
+++ src/LinkedList/ListaDeTarefas/ListaTarefas.java
@@ -1,3 +1,16 @@
+package LinkedList.ListaDeTarefas;
 public class ListaTarefas {
+    private Node primeiro;
+
+    // construtor
+    public ListaTarefas(){
+        this.primeiro = null;
+    }
+
+    public void adicionar(Tarefa t){
+        if (primeiro == null){
+            primeiro = new Node(t,null);
+        }
+    }
 
 }
