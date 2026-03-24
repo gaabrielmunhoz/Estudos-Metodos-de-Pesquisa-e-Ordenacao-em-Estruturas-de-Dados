8b2619c32e08957543fed7d17029753133508820
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-03-24 15:42:34 -0300

    Implementando inputs

--- src/LinkedList/ListaDeTarefas/Main.java
+++ src/LinkedList/ListaDeTarefas/Main.java
@@ -1,13 +1,31 @@
 package LinkedList.ListaDeTarefas;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("rodou");
+
+        System.out.println("------To-do List------");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Título: ");
+        String titulo = scanner.nextLine();
+        System.out.print("Data: ");
+        String data = scanner.nextLine();
+        System.out.print("Concluído(s/n): ");
+        String valor = scanner.nextLine();
+        scanner.close();
+
+        boolean concluido;
+        if ("s".equalsIgnoreCase(valor)) {
+            concluido = true;
+        } else {
+            concluido = false;
+        }
+
         ListaTarefas lista = new ListaTarefas();
-        Tarefa t1 = new Tarefa("Arrumar quarto", "24/03/2026", false);
-        Tarefa t2 = new Tarefa("Lavar banheiro", "24/03/2026", false);
-        lista.adicionar(t1);
-        lista.adicionar(t2);
+
+        Tarefa tarefa = new Tarefa(titulo, data, concluido);
+        lista.adicionar(tarefa);
         lista.listar();
 
     }
