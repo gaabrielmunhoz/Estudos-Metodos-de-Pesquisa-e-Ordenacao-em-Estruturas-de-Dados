bdef04c8912b74dce86ff05e1ae305942d55fd54
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-03-29 12:26:31 -0300

    Classe Livro criada

--- src/AtividadeFormativa3/Livro.java
+++ src/AtividadeFormativa3/Livro.java
@@ -1,4 +1,30 @@
 package AtividadeFormativa3;
 
 public class Livro {
+
+    // atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+
+    // contrutores
+    public Livro(String titulo, String autor, int anoPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    // métodos getters
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public int getAnoPublicacao(){
+        return anoPublicacao;
+    }
 }
