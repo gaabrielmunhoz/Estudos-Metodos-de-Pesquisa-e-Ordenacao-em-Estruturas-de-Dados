04436a83fd8b232cb2d0b3dae67f893f15898f75
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-03-29 12:44:31 -0300

    implementando livros em uma LinkedList na Main

--- src/AtividadeFormativa3/Main.java
+++ src/AtividadeFormativa3/Main.java
@@ -1,7 +1,27 @@
 package AtividadeFormativa3;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Criando a linked list
+        LinkedList<Livro> livro = new LinkedList<>();
+
+        // Adicionando livros na LinkedList
+        livro.add(new Livro("1984", "George Orwell", 1949));
+        livro.add(new Livro("Dom Casmurro", "Machado de Assis", 1899));
+        livro.add(new Livro("O Pequeno Príncipe", "Antoine de Saint-Exupéry", 1943));
+
+        // Laço for para listar os livros disponíveis
+
+        System.out.println("\n- - - Biblioteca - - -\n");
+        System.out.println("Lista de livros:");
+        for (Livro infoLivro : livro) {
+            System.out.println("\nTítulo: " + infoLivro.getTitulo());
+            System.out.println("Autor(a): " + infoLivro.getAutor());
+            System.out.println("Ano de publicação: " + infoLivro.getAnoPublicacao());
+        }
     }
 }
