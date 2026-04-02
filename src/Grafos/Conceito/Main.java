82e15c33d002fe122ff529eb4157a8990c122c13
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-04-02 14:10:01 -0300

    Estudos Grafos

---
+++ src/Grafos/Conceito/Main.java
@@ -0,0 +1,28 @@
+package Grafos.Conceito;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Primeiro nós importamos:");
+        System.out.println("import java.util.ArrayList;");
+        System.out.println("import java.util.LinkedList;");
+
+        System.out.println("\nIdeia mental - você vai ter:\n");
+        System.out.println("•Uma lista de cidades (nós)");
+        System.out.println("•Uma lista de conexões (arestas)\n");
+
+        System.out.println("Assim: ");
+        System.out.println("ArrayList<String> cidades;");
+        System.out.println("ArrayList<LinkedList<String>> rotas;");
+
+        ArrayList<String> cidades;
+        ArrayList<LinkedList<String>> rotas;
+
+        System.out.println("VOu criar uma classe Grafo em outro arquivo.");
+
+    }
+}
